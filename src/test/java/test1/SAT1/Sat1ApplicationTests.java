3bd6874c25b9df68cdb8c45f2d9c134a2fe71c3f
marlet1 <fktm2356@gmail.com>
2022-12-05 10:51:00 +0900

    2022-12-05 AM10:50

---
+++ src/test/java/test1/SAT1/Sat1ApplicationTests.java
@@ -0,0 +1,13 @@
+package test1.SAT1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sat1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
